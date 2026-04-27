541012d263bac9eed9b8cc48492fe1ea662fbca6
Kek3lvin <kelvin.franca@escolar.ifrn.edu.br>
2026-04-27 10:23:45 -0300

    capitulo 5.

--- projeto/loja/admin.py
+++ projeto/loja/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
+
 # Register your models here.
 from .models import *
-admin.site.register(Fabricante)
+
+class FabricanteAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao','preco', 'categoria',)
+    search_fields = ('Produto',)
+    fields = ('Produto', 'destaque', 'promocao','msgPromocao', 'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+
+admin.site.register(Fabricante,FabricanteAdmin)
 admin.site.register(Categoria)
-admin.site.register(Produto)
+admin.site.register(Produto, ProdutoAdmin)
+
 
 # código para acessar: http://127.0.0.1:8080/admin
 # código para rodar: python manage.py runserver 127.0.0.1:8080
+# código para ativar os scripts: venv\Scripts\activate
