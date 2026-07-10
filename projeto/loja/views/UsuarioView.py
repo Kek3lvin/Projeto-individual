e5b2a60b80418d341c5611657a5d7ac6db4b9d51
Kek3lvin <franca.kelvin@escolar.ifrn.edu.br>
2026-07-10 10:27:28 -0300

    continuar cap 9 slide 27

--- projeto/loja/views/UsuarioView.py
+++ projeto/loja/views/UsuarioView.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from loja.models import Usuario
+from loja.forms.UserUsuarioForm import UserUsuarioForm, UserForm
 def list_usuario_view(request, id=None):
 # carrega somente usuarios, não inclui os admin
     usuarios = Usuario.objects.filter(perfil=2)
@@ -9,3 +10,27 @@ def list_usuario_view(request, id=None):
     }
 
     return render(request, template_name='usuario/usuario.html', context=context, status=200)
+
+def edit_usuario_view(request):
+    usuario = get_object_or_404(Usuario, user=request.user)
+    usuarioForm = UserUsuarioForm(instance=usuario)
+    userForm = UserForm(instance=request.user)
+    context = {
+        'usuarioForm': usuarioForm,
+        'userForm': userForm
+        }
+    return render(request, template_name='usuario/usuario-edit.html', context=context,status=200)
+    emailUnused = True
+    if request.method == 'POST':
+        usuarioForm = UserUsuarioForm(request.POST, instance=usuario)
+        userForm = UserForm(request.POST, instance=request.user)
+        # Verifica se o e-mail que o usuário está tentando utilizar
+        # em seu perfil já existe em outro perfil
+        verifyEmail = Usuario.objects.filter(user__email=request.POST['email']).exclude(user__id=request.user.id).first()
+        emailUnused = verifyEmail is None
+    else:
+        usuarioForm = UserUsuarioForm(instance=usuario)
+        userForm = UserForm(instance=request.user)
+    if usuarioForm.is_valid() and userForm.is_valid() and emailUnused:
+        usuarioForm.save()
+        userForm.save()
