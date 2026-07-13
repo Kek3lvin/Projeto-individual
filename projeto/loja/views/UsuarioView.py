6bb049bb4da6652dac0c1a6311f9767bac97c901
Kek3lvin <franca.kelvin@escolar.ifrn.edu.br>
2026-07-13 09:43:19 -0300

    terminando cap 9

--- projeto/loja/views/UsuarioView.py
+++ projeto/loja/views/UsuarioView.py
@@ -2,25 +2,13 @@ from django.shortcuts import render, redirect, get_object_or_404
 from loja.models import Usuario
 from loja.forms.UserUsuarioForm import UserUsuarioForm, UserForm
 def list_usuario_view(request, id=None):
-# carrega somente usuarios, não inclui os admin
     usuarios = Usuario.objects.filter(perfil=2)
-
-    context = {
-    'usuarios': usuarios
-    }
-
+    context = {'usuarios': usuarios}
     return render(request, template_name='usuario/usuario.html', context=context, status=200)
-
 def edit_usuario_view(request):
     usuario = get_object_or_404(Usuario, user=request.user)
-    usuarioForm = UserUsuarioForm(instance=usuario)
-    userForm = UserForm(instance=request.user)
-    context = {
-        'usuarioForm': usuarioForm,
-        'userForm': userForm
-        }
-    return render(request, template_name='usuario/usuario-edit.html', context=context,status=200)
     emailUnused = True
+    message = None
     if request.method == 'POST':
         usuarioForm = UserUsuarioForm(request.POST, instance=usuario)
         userForm = UserForm(request.POST, instance=request.user)
@@ -34,3 +22,12 @@ def edit_usuario_view(request):
     if usuarioForm.is_valid() and userForm.is_valid() and emailUnused:
         usuarioForm.save()
         userForm.save()
+        message = { 'type': 'succes', 'text': 'Dados atualizados com sucesso'}
+    else:
+        if request.method == 'POST':
+            if emailUnused:
+                message = {'type': 'danger', 'text': 'Dados inválidos'}
+            else:
+                message = {'type': 'warning', 'text': 'E-mail já utilizado'}
+    context = {'usuarioForm': usuarioForm, 'userForm': userForm, 'message': message}
+    return render(request, template_name='usuario/usuario-edit.html', context=context, status=200)
